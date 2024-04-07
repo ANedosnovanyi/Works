97e87b212230708fb6d3bbe12dc1d110bd4ef411
ANedosnovanyi <eskander-conte@mail.ru>
2024-04-07 16:42:04 +0200

    Практическое задание Л8 выполнено.

--- Main.java
+++ Main.java
@@ -2,26 +2,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Animal cat = new Cat();
-        cat.run(200);
-        cat.swim(50);
-        Animal dog = new Dog();
-        Animal dog2 = new Dog();
-        dog.run(300);
-        dog.swim(10);
-        Cat.myCats();
-        Animal cat1 = new Cat();
-        System.out.println("Общее количество животных: " + Animal.getAnimalCount());
-        System.out.println("Количество котов: " + Cat.getCatCount());
-        System.out.println("Количество собак: " + Dog.getDogCount());
-
-        Circle circle = new Circle(12.0, "Red", "Black");
-        Rectangle rectangle = new Rectangle("Black", "Blue",8 , 6);
-        Triangle triangle = new Triangle("Black", "Yellow", 5.0,8 , 10);
-
-        GeometricFigure.printShapeInfo(circle);
-        GeometricFigure.printShapeInfo(rectangle);
-        GeometricFigure.printShapeInfo(triangle);
-
+        try {
+            int sum = MyArray.processArray(MyArray.array2);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
     }
 }
---
+++ MyArray.java
@@ -0,0 +1,36 @@
+public class MyArray {
+    public static String[][] array1 = {
+            {"1", "2", "3", "4"},
+            {"5", "6", "7", "8"},
+            {"9", "10", "11", "12"},
+            {"13", "14", "15", "16"}
+    };
+    public static String[][] array2 = {
+            {"1", "2", "3", "4"},
+            {"5", "6", "7", "8"},
+            {"9", "10", "11", "12"}
+    };
+
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Неверный размер массива");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    int number = Integer.parseInt(array[i][j]);
+                    sum += number;
+                } catch (NumberFormatException ex) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+        return sum;
+
+    }
+}
+
+
+
