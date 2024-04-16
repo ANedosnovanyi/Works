6b7b2406403c80246524841b22d2be8f9ef0e62c
ANedosnovanyi <eskander-conte@mail.ru>
2024-04-16 13:45:13 +0200

    Добавил факториальный калькулятор

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        FactorialCalculator.calculateFactorial(12);
+
+        int number = 12;
+        long factorial = FactorialCalculator.calculateFactorial(number);
+        System.out.println("Факториал числа " + number + " равен: " + factorial);
 
 
 
